35b7647ba75493292937f8917c34ce3941fb5c2b
KishenKumarrrrr <kishen1198@gmail.com>
2021-01-11 10:00:41 +0800

    Remove console.log in getPdf.js

--- backend/routes/pdf/getPdf.js
+++ backend/routes/pdf/getPdf.js
@@ -21,7 +21,6 @@ router.post("/create-pdf", (req, res) => {
 
 router.get("/get-pdf", (req, res) => {
   file = req.query.fileName;
-  console.log(file);
   fileLocation =  "./routes/pdf/" + file;
   res.download(fileLocation, (err) => {
                 if (err) console.log(err );
